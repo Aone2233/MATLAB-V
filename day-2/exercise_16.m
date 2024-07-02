5a0647af3ed33088c6c9d2ffc9806394df568e57
Aone2233 <liu-ly22@mails.tsinghua.edu.cn>
2024-07-02 10:58:07 +0800

    master

--- day-2/exercise_16.m
+++ day-2/exercise_16.m
@@ -20,7 +20,14 @@ fprintf('最大电阻电压是：%e，对应的频率是：%e。\n', V_R_max, f_max);
 
 % 由于fminbnd用于寻找最大电阻电压，绘图部分需要调整
 f_plot = 10:10:1000000; % 用于绘图的频率范围
-V_R_plot = arrayfun(V_R, f_plot); % 计算绘图用的电阻电压值
+V_R_plot = arrayfun(V_R, f_plot); % 计算绘图用的电阻电压值, arrayfun用于将函数应用于数组的每个元素
+
+% arrayfun的具体内容：
+% 基本语法：B = arrayfun(fun, A)
+% fun: 要应用于数组的函数句柄，指向想要应用在数组A中每个元素上的函数
+% A: 要应用于函数的数组
+% B: 输出数组，其元素为fun(A(i))的结果
+% 举例：arrayfun(@(x) x^2, [1, 2, 3]) 返回 [1, 4, 9]
 
 % 输出图像
 plot(f_plot, V_R_plot);
