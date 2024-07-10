03198b06b7869ad97ca0cbfbd3ab052b19042e79
Aone2233 <liu-ly22@mails.tsinghua.edu.cn>
2024-07-10 22:03:31 +0800

    master

--- Final-ex/example_of_teacher/private/final_test.m
+++ Final-ex/example_of_teacher/private/final_test.m
@@ -1,4 +1,4 @@
-function s_0 = final_test(t, p)
+function [s_0, h_0] = final_test(p, t)
 % 先设置一些参数
 % p_x = 16.53; % MPa P*的大小
 T_x = 1386; % T*的大小
@@ -12,9 +12,10 @@ ps = p_sat(t); % 饱和蒸汽压
 tao = T_x ./ t; % 无量纲温度
 for s = t >= 273.15 & t <= 623.15 & p >= ps & p <= 100 % 判断是否在区域1内
     for i = 1:34
-        s_0(s) = R * (tao .* Gibbs_tao_R1(p, t) - Gibbs_R1(p, t));
-        h_0(s) = R * t * Gibbs_tao_R1(p, t);
+        s_0(s) = R .* (tao .* Gibbs_tao_R1(p, t) - Gibbs_R1(p, t));
+        h_0(s) = R .* t .* tao .* Gibbs_tao_R1(p, t);
     end
 end
+
 
 
