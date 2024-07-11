2ca87d60096188034bc1848146a86ff7cb3c69d3
Aone2233 <liu-ly22@mails.tsinghua.edu.cn>
2024-07-11 22:07:44 +0800

    master

---
+++ Final-ex/example_of_teacher/Final_cal/Gibbs_tao_0_R2.m
@@ -0,0 +1,47 @@
+function gama_tao_0 = Gibbs_tao_0_R2(p,t)
+% Calculate the dimensionless differential Gibbs function to pi at region 2
+%
+%  Purpose:
+%    This function calculates the differential Gibbs function to pi at
+%    region 2
+%
+%  Record of revisions:
+%      Date       Programmer          Description of change
+%      ====       ==========          =====================
+%    2024/7/11   AONE 2233              Original code
+%
+% Define variables:
+%   t             -- Temperature in K
+%   p            -- Pressure in MPa, p and t should have same size.
+%   n            -- Numerical values of the coefficients of the equation (7)
+%   ii and ji  -- the exponents of the dimensionless Gibbs free energy
+%   ts           -- Saturation temperature at temperature t, K
+%   t23         -- temperature at the boundary of region 2 and 3 at
+%                  pressure p
+%   s             -- Scope of the range of region 1
+%                  ps <= p <= 100MPa
+%                  0  <=  t <= 623.15K
+%   i             -- loop index
+%  pai          -- dimensionless pressure
+%  tao          -- dimensionless temperature
+%
+% Numerical values of the coefficients
+n = [- 0.96927686500217D1, 0.10086655968018D2, ...
+    -0.56087911283020D-2, 0.71452738081455D-1, -0.40710498223928,...
+    0.14240819171444D1, -0.43839511319450D1, -0.28408632460772,...
+    0.21268463753307D-1];
+ji = [0, 1, -5, -4, -3, -2, -1, 2, 3];
+% Initialize gama to zeros as same size as t.
+gama_tao_0 = t * 0;
+% Calculate the saturation pressure at the temperature of t
+ts = t_sat(p);
+t23 = t_B23(p);
+% calculate the dimensionless pressure and temperature
+% pai = p;
+tao = 540 ./ t;
+% the range coverd by the equation
+s = ((t >= ts & t <= 623.15) | t >= t23);
+% Calculate the dimensionless differential Gibbs function to pi at region 1
+for i = 1:9
+    gama_tao_0(s) = gama_tao_0(s) + n(i) * ji(i) .* tao(s) .^ (ji(i) - 1);
+end
--- Final-ex/example_of_teacher/Final_cal/final_test.m
+++ Final-ex/example_of_teacher/Final_cal/final_test.m
@@ -9,10 +9,20 @@ s_0 = t * 0;
 h_0 = t * 0;
 ps = p_sat(t); % 饱和蒸汽压
 % pai = p ./ 16.53; % 无量纲压力
-tao = 1386 ./ t; % 无量纲温度
+tao_1 = 1386 ./ t; % 无量纲温度
+% 一区判定
 s = t >= 273.15 & t <= 623.15 & p >= ps & p <= 100; % 判断是否在区域1内
-s_0(s) = R .* (tao .* Gibbs_tao_R1(p, t) - Gibbs_R1(p, t));
-h_0(s) = R .* t .* tao .* Gibbs_tao_R1(p, t);
+s_0(s) = R .* (tao_1 .* Gibbs_tao_R1(p, t) - Gibbs_R1(p, t));
+h_0(s) = R .* t .* tao_1 .* Gibbs_tao_R1(p, t);
+% 二区判定
+ts = t_sat(p);
+t23 = t_B23(p);
+tao_2 = 540 ./ t;
+n = ((t >= ts & t <= 623.15) | t >= t23);
+s_0(n) = R .* (tao_2 .*(Gibbs_tao_0_R2(p, t) + Gibbs_tao_r_R2(p, t))-(Gibbs_0_R2(p, t) + Gibbs_r_R2(p, t)));
+h_0(n) = R .* t .* tao_2 .* (Gibbs_tao_0_R2(p, t) + Gibbs_tao_r_R2(p, t));
+% 三区判定
+
 
 
 
