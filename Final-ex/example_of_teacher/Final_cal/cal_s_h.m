86c9ec6bf06635059e640d2761bdf65cd5c216f9
Aone2233 <liu-ly22@mails.tsinghua.edu.cn>
2024-07-12 10:10:00 +0800

    master

--- Final-ex/example_of_teacher/Final_cal/cal_s_h.m
+++ Final-ex/example_of_teacher/Final_cal/cal_s_h.m
@@ -18,12 +18,13 @@ h_0(s) = R .* t .* tao_1 .* Gibbs_tao_R1(p, t);
 ts = t_sat(p);
 t23 = t_B23(p);
 tao_2 = 540 ./ t;
-n = ((t >= ts & t <= 623.15) | t >= t23) & t <= 1073.15;
+n = ((t>= ts & t<=623.15)|(t >= t23 & t >= 623.15) ) & t <= 1073.15 & p<=100;
 s_0(n) = R .* (tao_2 .* (Gibbs_tao_0_R2(p, t) + Gibbs_tao_r_R2(p, t))-(Gibbs_0_R2(p, t) + Gibbs_r_R2(p, t)));
 h_0(n) = R .* t .* tao_2 .* (Gibbs_tao_0_R2(p, t) + Gibbs_tao_r_R2(p, t));
 % 三区判定
-k = t >= 623.15 & t <= t_B23(p) & p >= p_B23(t) & p <= 100;
-rho = density_R3half(p, t);
+p23 = p_B23(t);
+k = t >= 623.15 & t < t23 & p >= p23 & p <= 100;
+rho = density_R3half(p, t); % 先计算出密度，后面用密度得到对应的焓和熵
 tao_3 = 647.096 ./ t;
 delta = rho./ 322;
 s_0(k) = R .* (tao_3 .* Helmz_phi_tao_R3(rho, t) - Helmz_phi_R3(rho, t));
